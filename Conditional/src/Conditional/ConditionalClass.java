2e5c92103b5dac3fb49eae758f6fe6cc98b3329a
Youngjae Jo <yicho9412@icloud.com>
2021-02-17 09:07:10 +0900

    chapter 04 conditional

---
+++ Conditional/src/Conditional/ConditionalClass.java
@@ -0,0 +1,29 @@
+package Conditional;
+
+public class ConditionalClass {
+	public static void main(String[] args) {
+		/*
+		 if (조건식) {
+		 	실행문;
+		 } else {
+		 	실행문;
+		 }
+		  */
+		/*
+		 while(조건식) {
+		 	실행문;
+		 }
+		 */
+		/*
+		 do {
+		 	실행문
+		 }
+		 while (조건식);
+		 */
+		/*
+		 for (초기화; 조건식; 증감식){
+		 	실행문;
+		 } 
+		 */
+	}
+}
