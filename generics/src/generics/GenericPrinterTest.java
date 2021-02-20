bda2a49ab503e71b7c3fe31079949172b7c797d3
Youngjae Jo <yicho9412@icloud.com>
2021-02-20 10:45:39 +0900

    chapter 12 object, interfaceex, class

---
+++ generics/src/generics/GenericPrinterTest.java
@@ -0,0 +1,18 @@
+package generics;
+
+public class GenericPrinterTest {
+//    public static void main(String[] args) {
+//        GenericPrinter<Powder> powderPrint = new GenericPrinter<Powder>();
+//
+//        powderPrint.setMaterial(new Powder());
+//        Powder powder = powderPrint.getMaterial();
+//        System.out.println(powderPrint);
+//
+//        GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<Plastic>();
+//
+//        plasticPrinter.setMaterial(new Plastic());
+//        Plastic plastic = plasticPrinter.getMaterial();
+//        System.out.println(plasticPrinter);
+//        plasticPrinter.printing();
+//    }
+}
